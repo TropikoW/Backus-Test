76d38bdc558339c30b4db0a555af9c3483cb1a90
Jovanny <jovannypersonal@gmail.com>
2023-11-16 09:18:35 -0500

    Some isues with the fifth an sixth testing

--- src/app/services/master.service.spec.ts
+++ src/app/services/master.service.spec.ts
@@ -1,21 +1,39 @@
-import { MasterService } from './master.service';
-import { ValueService } from './value.service';
-import { ValueServiceFake } from './value.service.fake';
+import { MasterService } from "./master.service";
 
-fdescribe('Test for a dependences services',()=>{
-  it('First test with valueservice of dependence',()=>{
+import { ValueService } from "./value.service";
+
+import { ValueServiceFake } from "./value.service.fake";
+
+describe('',()=>{
+  it('its first try test',()=>{
     let valueService = new ValueService;
     let masterService = new MasterService(valueService);
     expect(masterService.getValue()).toBe('Ronaldo');
   })
-  it('Second test with valueservice of dependence',()=>{
+  it('its second try test',()=>{
     let valueServiceFake = new ValueServiceFake;
     let masterService = new MasterService(valueServiceFake as unknown as ValueService);
     expect(masterService.getValue()).toBe('Ronaldo');
   })
-  it('Thirty test with valueservice of dependence',()=>{
-    let fake = { getValue :()=> 'Fake from object'};
-    let masterService = new MasterService(fake as ValueService);
-    expect(masterService.getValue()).toBe('Fake from object');
+  it('its third try test',()=>{
+    let valueServiceFake = {getValue : ()=>'Ronaldo fake'};
+    let masterService = new MasterService(valueServiceFake as ValueService);
+    expect(masterService.getValue()).toBe('Ronaldo fake');
+  })
+  it('its four try test',()=>{
+    let valueServiceSpy = jasmine.createSpyObj('ValueService',['getValue']);
+    valueServiceSpy.getValue.and.returnValue('Ronaldo fake spy');
+    let masterService = new MasterService(valueServiceSpy);
+    expect(masterService.getValue()).toBe('Ronaldo fake spy');
+  })
+  it('its five try test',()=>{
+    let valueServiceSpy = jasmine.createSpyObj('ValueService',['getValue']);
+    valueServiceSpy.getValue.and.returnValue('Ronaldo')
+    expect(valueServiceSpy.getValue).toHaveBeenCalled();
+  })
+  it('its six try test',()=>{
+    let valueServiceSpy = jasmine.createSpyObj('ValueService',['getValue']);
+    valueServiceSpy.getValue.and.returnValue('Ronaldo')
+    expect(valueServiceSpy.getValue).toHaveBeenCalledTimes(1);
   })
 })
